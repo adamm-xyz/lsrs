1ca3067f3f023cf1f91ce94c8b26c65c9d615d2b
Adam Montano <amontano495@gmail.com>
2025-02-26 15:07:31 -0800

    added colorizing based on file type from MIME guess

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::io;
 use std::env;
 use std::ffi::OsString;
 use std::fs::FileType;
-use colored::Colorize;
+use colored::{Colorize,ColoredString};
+use mime_guess::from_path;
 
 fn get_files(dir_path: &str) -> io::Result<Vec<(OsString,FileType)>> {
     //convert to Path obj
@@ -32,12 +33,44 @@ fn get_files(dir_path: &str) -> io::Result<Vec<(OsString,FileType)>> {
     Ok(file_list)
 }
 
-fn strify_files(files: &Vec<(OsString,FileType)>) -> Vec<String> {
+fn get_file_mime(filename: &str) -> String {
+    let file_mime = from_path(filename).first_or_octet_stream();
+    let file_mime_type = file_mime.type_();
+    file_mime_type.to_string()
+}
+
+fn set_file_color(filename: &str) -> ColoredString {
+   match get_file_mime(filename).as_str() {
+       "image" => {
+           filename.to_string().green()
+       }
+       "text" => {
+           filename.to_string().yellow()
+       }
+       "application" => {
+           filename.to_string().blue()
+       }
+       &_ => filename.to_string().magenta()
+   }
+}
+
+fn strify_files(files: &Vec<(OsString,FileType)>) -> Vec<ColoredString> {
     let mut file_strs = Vec::new();
     for file in files {
         let (file_name,file_type) = file;
         if let Some(file_str) = file_name.to_str() {
-            file_strs.push(file_str.to_string())
+            let mut file_str_colored = file_str.to_string().red();
+
+            if file_type.is_dir() {
+                file_str_colored = file_str_colored.bold();
+            }
+            else {
+                file_str_colored = set_file_color(file_str);
+                //file_str_colored = file_str_colored.blue();
+                //println!("{:?}",get_file_mime(file_str));
+            }
+            file_strs.push(file_str_colored);
+            //file_strs.push(file_str.to_string().red());
         }
     }
     file_strs
