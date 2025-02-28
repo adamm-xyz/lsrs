924866263e434e2fd05c61a6b1dfc19e7c9940c8
Adam Montano <amontano495@gmail.com>
2025-02-28 13:34:12 -0800

    added support for getting file size, experimental

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::path::Path;
 use std::io;
 use std::env;
 use std::ffi::OsString;
-use std::fs::FileType;
+use std::fs::{FileType,Metadata};
 use colored::{Colorize,ColoredString};
 use mime_guess::from_path;
 
-fn get_files(dir_path: &str, flags: &Flags) -> io::Result<Vec<(OsString,FileType)>> {
+fn get_files(dir_path: &str, flags: &Flags) -> io::Result<Vec<(OsString,FileType, Metadata)>> {
     //convert to Path obj
     let path = Path::new(dir_path);
 
@@ -29,12 +29,16 @@ fn get_files(dir_path: &str, flags: &Flags) -> io::Result<Vec<(OsString,FileType
         if flags.ignore_hidden {
             if !is_hidden_folder(entry.path().as_path()) {
                 if let Ok(file_type) = entry.file_type() {
-                    file_list.push((file_name,file_type));
+                    if let Ok(file_data) = entry.metadata() {
+                        file_list.push((file_name,file_type,file_data));
+                    }
                 }
             }
         } else {
             if let Ok(file_type) = entry.file_type() {
-                file_list.push((file_name,file_type));
+                if let Ok(file_data) = entry.metadata() {
+                    file_list.push((file_name,file_type,file_data));
+                }
             }
         }
 
@@ -73,10 +77,10 @@ fn set_file_color(filename: &str) -> ColoredString {
    }
 }
 
-fn strify_files(files: &Vec<(OsString,FileType)>) -> Vec<ColoredString> {
+fn strify_files(files: &Vec<(OsString,FileType, Metadata)>) -> Vec<ColoredString> {
     let mut file_strs = Vec::new();
     for file in files {
-        let (file_name,file_type) = file;
+        let (file_name,file_type,file_data) = file;
         if let Some(file_str) = file_name.to_str() {
             let mut file_str_colored = file_str.to_string().red();
 
@@ -84,8 +88,10 @@ fn strify_files(files: &Vec<(OsString,FileType)>) -> Vec<ColoredString> {
                 file_str_colored = file_str_colored.bold();
             }
             else {
-                file_str_colored = set_file_color(file_str);
-                //file_str_colored = file_str_colored.blue();
+                let file_size = file_data.len().to_string();
+                //file_str_colored = set_file_color(file_str) + String::from(" ") + file_size;
+                file_str_colored = format!("{} - {}",file_str,file_size).into();
+                file_str_colored = file_str_colored.blue();
                 //println!("{:?}",get_file_mime(file_str));
             }
             file_strs.push(file_str_colored);
