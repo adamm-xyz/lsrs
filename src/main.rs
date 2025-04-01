16fbf8f677192e9fd423a8304018dc159fd69e05
adamm-xyz <amontano495@gmail.com>
2025-04-01 07:36:11 -0700

    Added some more error checks in main

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,9 @@ use lsrs::entry::print_entries;
 fn main() {
     // Get flags and entries from given path on command line
     let flags = Flags::from_args();
-    let _ = print_entries(flags);
+    match print_entries(flags) {
+        Ok(()) => (),
+        Err(error) => eprintln!("{error}")
+    }
+
 }
